bd89e03466dde85b44c85d4c34ff0cd459a1e506
Alexander McDonald <am996653@wtstudents.wtamu.edu>
2018-09-12 11:10:09 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace secondtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
